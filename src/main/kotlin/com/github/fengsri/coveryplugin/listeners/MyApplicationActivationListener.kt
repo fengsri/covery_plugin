cae6ef7c2421d95dc736975799f8b1b2bd023e0c
GitHub Action <action@github.com>
2024-09-02 06:22:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/fengsri/coveryplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/fengsri/coveryplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fengsri.coveryplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
